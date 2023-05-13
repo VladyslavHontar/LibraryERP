056b3bb2adfa4b0226501a068311b01642e03197
Vlad <hontarvladie@gmail.com>
2023-05-13 21:49:25 +0100

    Changed Code style Added few more test cases in DynamicArrayTest

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,73 +1,78 @@
 import java.util.Arrays;
 
 public class DynamicArray {
-    private Object[] values = {};
-    private int size;
+  private Object[] values;
+  private int size;
 
-    public void add(Object value){
-        resizeIfNeeded();
-        values[size++] = value;
+  public DynamicArray() {
+    values = new Object[0];
+    size = 0;
+  }
+
+  public int getSize() {
+    return size;
+  }
+  public void add(Object value){
+    resizeIfNeeded();
+    values[size++] = value;
+  }
+  public void add(Object value, int position) {
+    if (position > size) {
+      System.err.println("IllegalPositionArgument: " + position);
+    } else if (position == size) {
+      add(value);
+    } else {
+      resizeIfNeeded();
+      System.arraycopy(values, position, values, position + 1, size - position);
+      values[position] = value;
+      size++;
     }
-    public void add(Object value, int position) {
-        if (position > size) {
-            System.err.println("IllegalPositionArgument: " + position);
-        } else if (position == size) {
-            add(value);
-        } else {
-            resizeIfNeeded();
-            System.arraycopy(values, position, values, position + 1, size - position);
-            values[position] = value;
-            size++;
-        }
+  }
+  public void set(Object value, int position) {
+    if (position < 0 || position >= size) {
+      System.err.println("IllegalPositionArgument: " + position);
+      return;
     }
-    public void set(Object value, int position) {
-        if (position < 0 || position >= size) {
-            System.err.println("IllegalPositionArgument: " + position);
-            return;
-        }
-        values[position] = value;
+    values[position] = value;
+  }
+  public boolean isEmpty() {
+    return size == 0;
+  }
+  public Object get(int position) {
+    if (position < 0 || position >= size) {
+      System.err.println("IllegalPositionArgument: " + position);
+      return null;
     }
-    public int getSize() {
-        return size;
+    return values[position];
+  }
+  public void remove(int position) {
+    if (position < 0 || position >= size) {
+      System.err.println("IllegalPositionArgument: " + position);
+    } else if (position == size - 1) {
+      values[position] = null;
+      size--;
+    } else {
+      System.arraycopy(values, position + 1, values, position, size - position - 1);
+      values[size - 1] = null;
+      size--;
     }
-    public boolean isEmpty() {
-        return size == 0;
+  }
+  public boolean contains(Object value) {
+    for (Object val : values) {
+      if (val.equals(value)) {
+        return true;
+      }
     }
-    public Object get(int position) {
-        if (position < 0 || position >= size) {
-            System.err.println("IllegalPositionArgument: " + position);
-            return null;
-        }
-        return values[position];
+    return false;
+  }
+  private void resizeIfNeeded() {
+    if (values.length == size) {
+      values = Arrays.copyOf(values, size * 2 + 1);
     }
-    public void remove(int position) {
-        if (position < 0 || position >= size) {
-            System.err.println("IllegalPositionArgument: " + position);
-        } else if (position == size - 1) {
-            values[position] = null;
-            size--;
-        } else {
-            System.arraycopy(values, position + 1, values, position, size - position - 1);
-            values[size - 1] = null;
-            size--;
-        }
-    }
-    public boolean contains(Object value) {
-        for (Object val : values) {
-            if (val.equals(value)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private void resizeIfNeeded() {
-        if (values.length == size) {
-            values = Arrays.copyOf(values, size * 2 + 1);
-        }
-    }
-    @Override
-    public String toString() {
-        return Arrays.toString(values);
-    }
+  }
+  @Override
+  public String toString() {
+    return Arrays.toString(values);
+  }
 }
 
--- src/DynamicArrayTest.java
+++ src/DynamicArrayTest.java
@@ -1,19 +1,26 @@
 import java.util.Arrays;
 
 public class DynamicArrayTest {
-    public static void main(String[] args) {
-        DynamicArray dynamicArray = new DynamicArray();
-        dynamicArray.add("A");
-        dynamicArray.add("B");
-        dynamicArray.add("C");
+  public static void main(String[] args) {
+    DynamicArray dynamicArray = new DynamicArray();
+    dynamicArray.add("A");
+    dynamicArray.add("B");
+    dynamicArray.add("C");
 
-        System.out.println(dynamicArray);
-        System.out.println(dynamicArray.getSize());
-        System.out.println(dynamicArray.isEmpty());
+    System.out.println(dynamicArray);
+    System.out.println(dynamicArray.getSize());
+    System.out.println(dynamicArray.isEmpty());
 
-        dynamicArray.remove(2);
-        dynamicArray.remove(0);
-        System.out.println(dynamicArray);
-    }
+    dynamicArray.remove(2);
+    dynamicArray.remove(0);
+    System.out.println(dynamicArray);
+
+    dynamicArray.set("Q", 0);
+    System.out.println(dynamicArray);
+
+    System.out.println(dynamicArray.contains("Q"));
+
+    dynamicArray.add("A", 1);
+  }
 }
 
