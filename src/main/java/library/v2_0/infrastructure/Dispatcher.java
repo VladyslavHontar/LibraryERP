4c2c3ddf4c06684a0e6363293916076c7df68bdd
Vlad <hontarvladie@gmail.com>
2023-07-27 23:39:31 +0100

    Dispatcher

---
+++ src/main/java/library/v2_0/infrastructure/Dispatcher.java
@@ -0,0 +1,35 @@
+package library.v2_0.infrastructure;
+
+import library.v2_0.controller.Controller;
+import library.v2_0.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dispatcher {
+
+  private final Map<String, Controller> controllersMapping = new HashMap<>();
+  private final Map<String, View> viewsMapping = new HashMap<>();
+
+  public void addController(String controllerName, Controller controller) {
+    controllersMapping.put(controllerName, controller);
+  }
+
+  public void addView(String viewName, View view) {
+    viewsMapping.put(viewName, view);
+  }
+
+  public ModelAndTarget dispatchRequest(String controller, Model inModel) {
+    Model outModel = new Model();
+    inModel.tryGet("user").ifPresent(user -> outModel.put("user", user));
+    String viewName = controllersMapping.get(controller).handle(inModel, outModel);
+    return new ModelAndTarget(outModel, viewName);
+  }
+
+  public ModelAndTarget dispatchResponse(String view, Model inModel) {
+    Model outModel = new Model();
+    inModel.tryGet("user").ifPresent(user -> outModel.put("user", user));
+    String viewName = viewsMapping.get(view).update(inModel, outModel);
+    return new ModelAndTarget(outModel, viewName);
+  }
+}
