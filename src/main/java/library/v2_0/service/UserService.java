86079a7d8a0c73f0c637cc8511c06d356e4af74b
Vlad <hontarvladie@gmail.com>
2023-08-10 19:34:31 +0100

    BCrypt added to crypt the passwords and few more minor changes so you cant create user with no username and password

--- src/main/java/library/v2_0/service/UserService.java
+++ src/main/java/library/v2_0/service/UserService.java
@@ -1,11 +1,13 @@
 package library.v2_0.service;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import library.v2_0.domain.User;
 import library.v2_0.exceptions.AuthenticationException;
 import library.v2_0.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -16,36 +18,53 @@ public class UserService {
 
   private final UserRepository repository;
 
-  public User authenticate(String username, String password) {
-    User user = repository.findByUsername(username).orElseThrow(NoSuchElementException::new);
+  public User authenticate(String username, char[] password) {
+    try {
+      User user = repository.findByUsername(username).orElseThrow(NoSuchElementException::new);
 
-    if (!user.getPassword().equals(password)) {
-      log.warn("User [{}] failed to authenticate!", username);
-      throw new AuthenticationException();
+      if (!BCrypt.verifyer().verify(password, user.getPasswordHash()).verified) {
+        log.warn("User [{}] failed to authenticate!", username);
+        throw new AuthenticationException();
+      }
+      log.info("User [{}] successfully authenticated", username);
+      return user;
+    } finally {
+      Arrays.fill(password, '\u0000');
     }
-
-    log.info("User [{}] successfully authenticated", username);
-    return user;
   }
 
-  public User add(User admin, String username, String password, User.Type type) {
-    log.info("Admin [{}] trying to add a user [{}] with type [{}]", admin, username, type);
+  public User add(User admin, String username, char[] password, User.Type type) {
+    try {
+      log.info("Admin [{}] trying to add a user [{}] with type [{}]", admin, username, type);
 
-    Optional<User> user = repository.findByUsername(username);
-    if (user.isPresent()) {
-      log.error("Admin [{}] trying to add a user with existing username [{}]", admin, username);
-      throw new IllegalStateException();
+      Optional<User> user = repository.findByUsername(username);
+      if (username.isBlank()) {
+        log.error("Admin [{}] trying to add a user with no username", admin);
+        throw new IllegalStateException();
+      }
+      if (user.isPresent()) {
+        log.error("Admin [{}] trying to add a user with existing username [{}]", admin, username);
+        throw new IllegalStateException();
+      }
+      if (password.length < 8) {
+        log.error("Admin [{}] trying to add a user with a password shorter than 8 characters", admin);
+        throw new IllegalStateException();
+      }
+
+      String passwordHash = BCrypt.withDefaults().hashToString(12, password);
+
+      User newUser = repository.save(User.builder()
+                                         .username(username)
+                                         .passwordHash(passwordHash)
+                                         .type(type)
+                                         .build());
+
+      log.info("Admin [{}] successfully added a user [{}]", admin, newUser.getId());
+
+      return newUser;
+    } finally {
+      Arrays.fill(password, '\u0000');
     }
-
-    User newUser = repository.save(User.builder()
-                                       .username(username)
-                                       .password(password)
-                                       .type(type)
-                                       .build());
-
-    log.info("Admin [{}] successfully added a user [{}]", admin, newUser.getId());
-
-    return newUser;
   }
 
   public User delete(User admin, long userId) {
